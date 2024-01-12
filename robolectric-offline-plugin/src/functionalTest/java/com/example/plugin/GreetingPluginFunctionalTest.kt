c58130696aa56f6d00e96c4291e4b23e24d14970
utzcoz <utzcoz@outlook.com>
2024-01-12 22:35:59 +0800

    Convert all Java files to Kotlin
    Signed-off-by: utzcoz <utzcoz@outlook.com>

---
+++ robolectric-offline-plugin/src/functionalTest/java/com/example/plugin/GreetingPluginFunctionalTest.kt
@@ -0,0 +1,38 @@
+package com.example.plugin
+
+import org.gradle.testkit.runner.BuildResult
+
+class GreetingPluginFunctionalTest {
+    @Test
+    @Throws(IOException::class)
+    fun canRunTask() {
+        // Setup the test build
+        val projectDir: File = File("build/functionalTest")
+        Files.createDirectories(projectDir.toPath())
+        writeString(File(projectDir, "settings.gradle"), "")
+        writeString(
+            File(projectDir, "build.gradle"),
+            "plugins {" +
+                    "  id('com.example.plugin.greeting')" +
+                    "}"
+        )
+
+        // Run the build
+        val result: BuildResult = GradleRunner.create()
+            .forwardOutput()
+            .withPluginClasspath()
+            .withArguments("greet")
+            .withProjectDir(projectDir)
+            .build()
+
+        // Verify the result
+        assertTrue(result.getOutput().contains("Hello from plugin 'com.example.plugin.greeting'"))
+    }
+
+    @Throws(IOException::class)
+    private fun writeString(file: File, string: String) {
+        FileWriter(file).use { writer ->
+            writer.write(string)
+        }
+    }
+}
---
+++ robolectric-offline-plugin/src/main/java/com/example/plugin/GreetingPlugin.kt
@@ -0,0 +1,12 @@
+package com.example.plugin
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class GreetingPlugin : Plugin<Project?> {
+    fun apply(project: Project) {
+        project.getTasks().register("greet") { task ->
+            task.doLast { s -> System.out.println("Hello from plugin 'com.example.plugin.greeting'") }
+        }
+    }
+}
